b8d52c1f22ef971281725fdc242ef8d3b0420bc9
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-13 11:02:53 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,159 @@
+
+// 1
+
+// var userName;
+
+
+
+
+// 2
+
+// var myName="Zeeshan Ahmed";
+// console.log(myName);
+
+
+// 3
+
+
+// var message="Hello World";
+// alert(message)
+
+
+// 4
+
+
+// var name="Zeeshan";
+// var age=18;
+// var isDeveloper=true;
+
+// alert(name);
+// alert(age);
+// alert(isDeveloper);
+
+
+
+
+
+
+// 5
+
+
+// alert("Pizza \n Pizz \n Piz \n Pi \n P")
+
+
+// 6
+
+
+// var email="shani2103792@gmail.com";
+
+// alert("My Email Address is " + email)
+
+
+
+
+
+
+
+// 7
+
+
+// var book = "A smarter way to learn java script";
+// alert("I am Trying to Learn from the Book  " + book);
+
+
+
+// 8
+
+
+// var display="Yah! I can write HTML content through JavaScript";
+// document.write(display)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
